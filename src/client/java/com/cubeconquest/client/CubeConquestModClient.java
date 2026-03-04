062b337bd8144cd97a96974821bfcb2c17252a03
junyeol\LEE <ljy0108855@gmail.com>
2026-03-04 16:14:44 +0900

    Initial Fabric 1.20.1 project

---
+++ src/client/java/com/cubeconquest/client/CubeConquestModClient.java
@@ -0,0 +1,10 @@
+package com.cubeconquest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CubeConquestModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/cubeconquest/CubeConquestMod.java
@@ -0,0 +1,10 @@
+package com.cubeconquest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CubeConquestMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
